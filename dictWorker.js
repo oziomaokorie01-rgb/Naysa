2cbdc158d212893a3f9bc0747e7614518e1a707e
Ozzy-sensei <oziomaokorie01@gmail.com>
2026-06-29 22:35:38 +0100

    Refactor database initialization and population logic

--- dictWorker.js
+++ dictWorker.js
@@ -1,27 +1,45 @@
-// dictWorker.js - Runs entirely in the background
+// Background Worker Thread
 self.onmessage = async function(e) {
-    if (e.data.action === 'SEED_DATABASE') {
-        try {
-            const res = await fetch("https://raw.githubusercontent.com/matthewreagan/Web-Words/master/words.json");
-            const rawData = await res.json();
+    if (e.data.action === 'INITIALIZE_DB') {
+        const request = indexedDB.open("SenseiiCompleteDB", 1);
+
+        request.onupgradeneeded = function(evt) {
+            let dbInstance = evt.target.result;
+            if (!dbInstance.objectStoreNames.contains("words")) {
+                dbInstance.createObjectStore("words", { keyPath: "word" });
+            }
+        };
+
+        request.onsuccess = function(evt) {
+            const db = evt.target.result;
             
-            // Open DB and write records cleanly away from the UI thread
-            const request = indexedDB.open("SenseiiCompleteDB", 1);
-            request.onsuccess = function(evt) {
-                const db = evt.target.result;
-                const tx = db.transaction(["words"], "readwrite");
-                const store = tx.objectStore("words");
-                
-                Object.keys(rawData).forEach(key => {
-                    store.put({ word: key.toLowerCase(), definition: rawData[key] });
-                });
-                
-                tx.oncomplete = () => {
+            // Check if population is needed
+            const transaction = db.transaction(["words"], "readonly");
+            const countRequest = transaction.objectStore("words").count();
+
+            countRequest.onsuccess = async function() {
+                if (countRequest.result === 0) {
+                    try {
+                        const res = await fetch("https://raw.githubusercontent.com/matthewreagan/Web-Words/master/words.json");
+                        const rawData = await res.json();
+
+                        const writeTx = db.transaction(["words"], "readwrite");
+                        const writeStore = writeTx.objectStore("words");
+
+                        Object.keys(rawData).forEach(key => {
+                            writeStore.put({ word: key.toLowerCase(), definition: rawData[key] });
+                        });
+
+                        writeTx.oncomplete = () => {
+                            self.postMessage({ status: 'READY' });
+                        };
+                    } catch (err) {
+                        self.postMessage({ status: 'ERROR', message: err.message });
+                    }
+                } else {
                     self.postMessage({ status: 'READY' });
-                };
+                }
             };
-        } catch (err) {
-            self.postMessage({ status: 'ERROR', error: err.message });
-        }
+        };
     }
 };
